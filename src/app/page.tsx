74816a803b3a82f1e356cc69da9d1f102c5ef4b8
xrdn00 <reydan39@gmail.com>
2024-07-16 21:07:46 +0800

    under construction react app

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      this is page 1 of my website.
+    </main>
+  );
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function page2() {
+  return (
+    <main>
+      <h1>Projects</h1>
+      This is the page 2 of my website
+    </main>
+  )
+}
